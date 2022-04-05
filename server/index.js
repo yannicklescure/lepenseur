892c2de9d6ebd94f81b8089bf1366c273f7d3d1d
Yannick Lescure <yannick.lescure@gmail.com>
2022-04-05 15:25:23 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const express = require("express");
+const morgan = require("morgan");
+const morgan = require("cors");
+const morgan = require("helmet");
+
+const PORT = 8000;
+
+express()
+  .use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  })
+  .use(morgan("tiny"))
+  .use(helmet())
+  .use(cors())
+  .use(express.static("./server/assets"))
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use("/", express.static(__dirname + "/"))
+
+  .get("/bacon", (req, res) => res.status(200).json("🥓"))
+
+  .listen(PORT, () => console.info(`Listening on port ${PORT}`));
